57f6059762bb19b6bbca2111be36625c6c64760d
IPattyI <bilivrayka@gmail.com>
2024-08-29 02:13:43 +0300

    Shitposted

--- src/main/java/net/bilivrayka/callofequestria/item/custom/PlushItem.java
+++ src/main/java/net/bilivrayka/callofequestria/item/custom/PlushItem.java
@@ -6,6 +6,7 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -26,51 +27,49 @@ public class PlushItem extends BlockItem {
     private static final Random rnd = new Random();
     protected final SoundEvent sound2;
     protected final SoundEvent sound3;
+    protected final SoundEvent stereo_sound2;
+    protected final SoundEvent stereo_sound3;
 
-    public PlushItem(SoundEvent sound2, SoundEvent sound3, Block pBlock, Properties pProperties) {
+    public PlushItem(SoundEvent sound2, SoundEvent sound3,
+                     SoundEvent stereoSound2, SoundEvent stereoSound3, Block pBlock, Properties pProperties) {
         super(pBlock, pProperties);
         this.sound2 = sound2;
         this.sound3 = sound3;
+        this.stereo_sound2 = stereoSound2;
+        this.stereo_sound3 = stereoSound3;
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-
-        pPlayer.getCooldowns().addCooldown(this, 10);
+        // У игроков звуки воспроизводятся разные
+        //мб потом пофикшу
         float pitch = rnd.nextFloat(1,1.15f);
-        SoundEvent sound = ModSounds.STEREO_SQUEE1.get();
-
-        switch (rnd.nextInt(3)+1){
-            case 2 :
-                sound = sound2;
-                break;
-            case 3 :
-                sound = sound3;
-                break;
-
-        }
-        /*
-        pLevel.playSound(null, pPlayer.getX(),pPlayer.getY()+1,pPlayer.getZ(),
-                sound, SoundSource.PLAYERS, 1.0F, pitch);
-
-         */
-
-        // если вдруг захочу зарегать все звуки дважды, моно и стерео
-        // это позволит плюшам предметам воспроизводить звук в игроке, чтобы от звука нельзя было убежать
-        // но впизду это делать сейчас
+        SoundEvent sound = ModSounds.SQUEE1.get();
+        SoundEvent stereoSound = ModSounds.STEREO_SQUEE1.get();
+        pPlayer.getCooldowns().addCooldown(this, 10);
         if(!pLevel.isClientSide){
+
+            switch (rnd.nextInt(3)+1){
+                case 2 :
+                    sound = sound2;
+                    stereoSound = stereo_sound2;
+                    break;
+                case 3 :
+                    sound = sound3;
+                    stereoSound = stereo_sound3;
+                    break;
+
+            }
             pLevel.playSound(pPlayer, pPlayer.getOnPos(),
                     sound, SoundSource.PLAYERS, 1.0F, pitch);
+
         }
-        //pPlayer.playSound(sound,1.0f, pitch);
-        pLevel.playLocalSound(pPlayer.getOnPos(),sound,SoundSource.PLAYERS, 1.0f, pitch,true);
-
-
+        pLevel.playLocalSound(pPlayer.getOnPos(),stereoSound,SoundSource.PLAYERS, 1.0f, pitch,true);
         return super.use(pLevel, pPlayer, pUsedHand);
     }
 
     @Override
     public boolean canEquip(ItemStack stack, EquipmentSlot slot, Entity entity) {
-        return slot == EquipmentSlot.HEAD; // Разрешаем экипировку только на голову
+        return slot == EquipmentSlot.HEAD;
     }
 }
